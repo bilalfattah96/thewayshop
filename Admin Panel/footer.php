ce4a4c5de525f177c6a81dc8fd68935985364dfa
bilal <bilal@yahoo.com>
2024-12-06 13:06:19 +0500

    new

---
+++ Admin Panel/footer.php
@@ -0,0 +1,16 @@
+<footer class="py-4 bg-light mt-auto">
+    <div class="container-fluid px-4">
+        <div class="d-flex align-items-center justify-content-between small">
+            <div class="text-muted">Copyright &copy; 2023. Design & Developed By Bilal Fattah</div>
+        </div>
+    </div>
+</footer>
+
+</div>
+<!-- Right Side Content End -->
+</div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+<script src="js/scripts.js"></script>
+</body>
+
+</html>
---
+++ Admin Panel/home.php
@@ -0,0 +1,14 @@
+<?php
+include('header.php');
+
+?>
+<!-- Right Side Content Start -->
+<div id="layoutSidenav_content">
+    <main>
+        <div class="container-fluid px-4">
+            <h2 class="mt-4 text-center text-uppercase" style="letter-spacing:4px">Ecommerce Website</h2>
+        </div>
+    </main>
+    <?php
+    include('footer.php');
+    ?>
